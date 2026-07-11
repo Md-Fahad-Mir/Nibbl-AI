e5838af768589eb3b57071295022f7df72cec15e
Md Fahad Mir <fahad1000mir@gmail.com>
2026-07-11 14:18:23 +0600

    Remove AI service from monorepo

--- infrastructure/terraform/bootstrap/variables.tf
+++ infrastructure/terraform/bootstrap/variables.tf
@@ -26,7 +26,6 @@ variable "state_bucket_name" {
 variable "ecr_repositories" {
   description = "ECR repositories to create (one per service)"
   type        = list(string)
-  # backend + ai are in scope now; add "nibblai-landing", "nibblai-admin"
-  # here as those services are added to the monorepo.
-  default = ["nibblai-backend", "nibblai-ai"]
+  # Add new service repositories here as those services are added to the monorepo.
+  default = ["nibblai-backend"]
 }
